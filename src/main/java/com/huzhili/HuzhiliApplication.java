16e90b244036dc3e994f58b43c099f754a93f152
huzhili <584142342@qq.com>
2023-12-08 14:09:53 +0800

    初始化

---
+++ src/main/java/com/huzhili/HuzhiliApplication.java
@@ -0,0 +1,13 @@
+package com.huzhili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuzhiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuzhiliApplication.class, args);
+	}
+
+}
